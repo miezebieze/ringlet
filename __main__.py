c2cdf549f34dd74beaadc8451b241376e4de83f8
Aster* <git@miezys.de>
2020-06-11 09:51:42 +0200

    cleaned up

--- __main__.py
+++ __main__.py
@@ -1,94 +1,46 @@
 #!/bin/python
 import time
-import pygame
 import sys
 import math
 
-def process_events ():
-    keepopen = True
-    for e in pygame.event.get ():
-        if e.type == pygame.QUIT:
-            keepopen = False
-        elif e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
-            keepopen = False
-    return keepopen
-
-class Window:
-
-    def __init__ (self,size):
-        self.width = size[0]
-        self.height = size[1]
-
-    def open (self):
-        pygame.init ()
-        self.surface = pygame.display.set_mode (self.size)
-
-    @property
-    def size (self):
-        return self.width,self.height
-
-    @property
-    def centre (self):
-        return int (self.width/2),int (self.height/2)
-
-    def fill (self,colour=(0,0,0)):
-        self.surface.fill (colour)
-
-    def update (self):
-        pygame.display.flip ()
-
-    def draw (self,object_):
-        object_.draw (self.surface,*self.centre)
-
-class Circle:
-
-    def __init__ (self,ri,ro,segments=8):
-        self.ri = ri
-        self.ro = ro
-        self.segments = segments
-        self.colour = (222,222,222)
-
-    def draw (self,surface,x=0,y=0):
-        pygame.draw.circle (surface,self.colour,(x,y),self.ro,1)
-        pygame.draw.circle (surface,self.colour,(x,y),self.ri,1)
-
-        for i in range (self.segments):
-            rad = math.radians (360 / self.segments * i+22.5)
-            cos_,sin_ = math.cos (rad),math.sin (rad)
-            xa = self.ro * cos_ + x
-            ya = self.ro * sin_ + y
-            xb = self.ri * cos_ + x
-            yb = self.ri * sin_ + y
-            pygame.draw.line (surface,self.colour,(xa,ya),(xb,yb),1)
+from pypygame import Window, Ring
+from vec2d import Vec2d
 
 colours = ['blue','magenta','red','orange','grey','white','yellow','green']
 
 window = Window (size=(200,200))
-circle = Circle (50,100)
+ring = Ring (window,rinner=50,router=100)
 
 window.open ()
-pygame.mouse.set_pos (window.centre)
+window.center_mouse ()
 
-switched = False
-keepopen = True
-while keepopen:
-    keepopen = process_events ()
-    mousex,mousey = pygame.mouse.get_pos ()
-    mousex,mousey = mousex-window.width/2,mousey-window.height/2
-    if math.sqrt (mousex**2 + mousey**2) > circle.ri:
-        deg = math.degrees (math.atan2 (mousex,mousey))
-        c = colours[int ((deg - (180 / circle.segments))/ (360 / circle.segments))]
-        circle.colour = pygame.color.THECOLORS[c]
-        time.sleep (0.5)
-        switched = True
+switch = True
+while window.isopen:
+    window.process_events ()
 
-    window.fill ()
-    window.draw (circle)
-    window.update ()
-    if switched:
-        pygame.mouse.set_pos (window.centre)
-        switched = False
+    mouse = Vec2d (window.mousepos)
+    mouse -= window.centre
+    mouse.angle += 90
+    mouse.angle += 180 / ring.segments
+    if mouse.angle < 0:
+        angle = 360+mouse.angle
+    else:
+        angle = mouse.angle
+
+    if mouse.length > ring.ri:
+        segment = int (angle / (360 / ring.segments))
+        ring.colour = colours[segment]
+        switch = True
+        time.sleep (0.1)
+
+    #window.fill ()
+
+    if switch:
+        ring.draw ()
+        window.update ()
+        window.center_mouse ()
+        switch = False
+    time.sleep (0.03)
 
 else:
-    pygame.quit ()
     sys.exit ()
---
+++ pypygame.py
@@ -0,0 +1,75 @@
+import pygame
+import math
+
+colour = pygame.color.THECOLORS
+
+class Window:
+    isopen = True
+
+    def __init__ (self,size):
+        self.width = size[0]
+        self.height = size[1]
+
+    def open (self):
+        pygame.init ()
+        self.surface = pygame.display.set_mode (self.size)
+
+    @property
+    def size (self):
+        return self.width,self.height
+
+    @property
+    def centre (self):
+        return int (self.width/2),int (self.height/2)
+
+    def fill (self,colour=(0,0,0)):
+        self.surface.fill (colour)
+
+    def update (self):
+        pygame.display.flip ()
+
+    def draw (self,object_):
+        object_.draw (self.surface,*self.centre)
+
+    def process_events (self):
+        for e in pygame.event.get ():
+            if e.type == pygame.QUIT:
+                self.isopen = False
+            elif e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+                self.isopen = False
+
+    def center_mouse (self):
+        pygame.mouse.set_pos (self.centre)
+
+    @property
+    def mousepos (self):
+        return pygame.mouse.get_pos ()
+
+    def close (self):
+        pygame.quit ()
+
+class Ring:
+
+    def __init__ (self,window,rinner,router,segments=8):
+        self.window = window
+        self.ri = rinner
+        self.ro = router
+        self.segments = segments
+        self.colour = 'white'
+
+    def draw (self):
+        pygame.draw.circle (self.window.surface,colour[self.colour],self.window.centre,self.ro,1)
+        pygame.draw.circle (self.window.surface,colour[self.colour],self.window.centre,self.ri,1)
+
+        # draw lines
+        for i in range (self.segments):
+            rad = math.radians (360 / self.segments * i + 180 / self.segments)
+            cos,sin = math.cos (rad),math.sin (rad)
+            pygame.draw.line (
+                    self.window.surface,colour[self.colour],
+                    (self.ro * cos + self.window.centre[0],self.ro * sin + self.window.centre[1]),
+                    (self.ri * cos + self.window.centre[0],self.ri * sin + self.window.centre[1]),
+                    1)
+
+    def left_inner (self):
+        ...
